49cf4e02e9f82a4d5831bf757264f15f29288e33
Tinashe Jena <90519516+jena2010@users.noreply.github.com>
2024-04-29 11:20:40 +0200

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -18,10 +18,7 @@ const About = () => {
 		  I recently completed my BSc Computer Science Degree at University of the Western Cape 
         </p>
         <p className="a-desc">
-          I would like to join SovTech as a developer to kick start my career and contribute to the growth of the company.
-		  SovTech is a leading company in custom software dvelopment in South Africa ,with mutiple awards ,so it will be a great honor for me 
-		  to be part of such a company .I am someone who believes technology can solve any problems and that what motivated me to persue a career 
-		  in software development so that i can contribute to solving problems and make the world a better place.
+         
         </p>
         
       </div>
